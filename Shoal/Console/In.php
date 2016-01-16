19de5504b9a2c9e2c93c110f2327d42820ad8468
David Cloutman <david.cloutman@clorox.com>
2016-01-15 17:22:31 -0800

    Added a class to handle user input from the console.

---
+++ Shoal/Console/In.php
@@ -0,0 +1,33 @@
+<?php
+namespace Shoal\Console;
+
+/** A simple class to get line input from the console.
+ */
+class In {
+	protected $in;
+	protected $last_input = '';
+	protected $max_len = 8196;
+
+	public function __construct() {
+		$this->in = fopen('php://stdin', 'r');
+	}
+
+	/** Get a line of input.
+	 *  @param string $prompt An optional line of input.
+	 *  @return string The line of input with leading and trailing whitespace removed.
+	 */
+	public function getln ( $prompt = '' ) {
+		echo $prompt;
+		$text = trim(fgets($this->in, $this->max_len));
+
+		$this->in = $text;
+	return $this->in;
+	}
+
+	public function __toString() {
+		return $this->in;
+	}
+}
+
+echo (new In())->getln('Enter a string: ');
+echo "\n";
