8700d517c94d665c269924fa0eda9e20d052d976
David Cloutman <david.cloutman@lampfire.com>
2016-04-12 13:46:09 -0700

    Split the original TreeNode class into a BaseClass for use as a subclass, and a paired interface and trait to provide a default implementation when inheriting the base class is not an acceptable design constraint.

---
+++ Shoal/Struct/Tree/TreeNode.php
@@ -0,0 +1,44 @@
+<?php
+namespace Shoal\Struct\Tree;
+
+
+/** This interface defines functionality for the TreeNode type. A defeault implementation the
+ *  TreeNodeTrait trait. This interface, combined with that trait, can be added to any class to
+ *  make it a TreeNode. The BaseTreeNode class demonstrates this implementation technique.
+ */
+interface TreeNode {
+	/** This construction optionally sets the parent of the new node.
+	 *  @param TreeNode $parent
+	 */
+	public function __construct (TreeNode $parent = null);
+
+	/** Adds a child node to this node and points the child node's parent member to this node.
+	 *  @param Treenode $newChild
+	 */
+	public function addChild (TreeNode $newChild);
+
+	/** Assigns this node a new parent node.
+	 *  @param TreeNode $parent
+	 */
+	public function setParent(TreeNode $parent);
+
+	/** Returns the parent node. The return value will be null if the node has no parent. (E.g. The node is a the top of the tree.)
+	 *  @return TreeNode|null
+	 */
+	public function getParent();
+
+	/** Returns an array of the node's children.
+	 *  @return array An array of TreeNodes.
+	 */
+	public function getChildren();
+
+	/** Applies to callback function to the node and then its children through recursion.
+	 *  @param callable $func The function to be applied. The function must one parameter which will store each instance of TreeNode to which the callback is passed.
+	 */
+	public function applyFunctionToTree (callable $func);
+
+	/** Returns an array of the node's ancestors.
+	 *  @return Array An array of TreeNode objects.
+	 */
+	public function getAncestors();
+}
---
+++ Shoal/Struct/Tree/TreeNodeTrait.php
@@ -0,0 +1,78 @@
+<?php
+namespace Shoal\Struct\Tree;
+
+/** This trait provides a default implementation of the TreeNode interface. It provides logic for a node 
+ *  intended for use in a tree type data structure. 
+ *  @see TreeNode
+ */
+trait TreeNodeTrait {
+	protected $parent = null;
+	protected $children = [];
+
+	/** This construction optionally sets the parent of the new node.
+	 *  @param TreeNode $parent
+	 */
+	public function __construct (TreeNode $parent = null) {
+		$this->parent = $parent;
+	}
+
+	/** Adds a child node to this node and points the child node's parent member to this node.
+	 *  @param TreeNode $newChild
+	 */
+	public function addChild (TreeNode $newChild) {
+		$newChild->setParent($this);
+		$this->children[] = $newChild;
+	}
+
+	/** Assigns this node a new parent node.
+	 *  @param TreeNode $parent
+	 */
+	public function setParent(TreeNode $parent) {
+		$this->parent = $parent;
+	}
+
+	/** Returns the parent node. The return value will be null if the node has no parent. (E.g. The node is a the top of the tree.)
+	 *  @return TreeNode|null
+	 */
+	public function getParent() {
+		return $this->parent;
+	}
+
+	/** Returns an array of the node's children.
+	 *  @return array An array of TreeNodes.
+	 */
+	public function getChildren() {
+		return $this->children;
+	}
+
+	/** Applies to callback function to the node and then its children through recursion.
+	 *  @param callable $func The function to be applied. The function must one parameter which will store each instance of TreeNode to which the callback is passed.
+	 */
+	public function applyFunctionToTree (callable $func) {
+		$func($this);
+
+		foreach ($this->children as $child) {
+			$child->applyFunctionToTree($func);
+		}
+	}
+
+	/** Returns an array of the node's ancestors.
+	 *  @return Array An array of TreeNode objects.
+	 */
+	public function getAncestors() {
+		return $this->recurseAncestors();
+	}
+
+	/** A helper function for getAncestors.
+	 *  @param array An array of ancestor TreeNode objects.
+	 *  @return array
+	 */
+	protected function recurseAncestors(array $ancestors = []) {
+		if (null !== $this->parent) {
+			$ancestors[] = $this->parent;
+			$this->parent->recurseAncestors($ancestors);
+		}
+
+		return $ancestors;
+	}
+}
