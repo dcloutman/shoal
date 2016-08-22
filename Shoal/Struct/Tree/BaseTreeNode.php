7e5926edd821e8b6bc4ff428e5a60bcb4b9d3d39
David Cloutman <david.cloutman@lampfire.com>
2016-08-22 11:52:20 -0700

    Removed test code from class as this belongs in a unit test.

--- Shoal/Struct/Tree/BaseTreeNode.php
+++ Shoal/Struct/Tree/BaseTreeNode.php
@@ -9,38 +9,3 @@ namespace Shoal\Struct\Tree;
 class BaseTreeNode implements TreeNode {
 	use TreeNodeTrait;
 }
-
-
-/*
-//// Test code.
-
-require_once (__DIR__ . '/TreeNode.php');
-require_once (__DIR__ . '/TreeNodeTrait.php');
-
-
-class IntTreeNode extends BaseTreeNode {
-	public $num = 0;
-}
-
-
-$trunk = new IntTreeNode();
-for ($i = 0; $i < 3; $i++) { 
-	$trunk->addChild(new IntTreeNode());
-}
-
-var_dump($trunk);
-
-$trunk->applyFunctionToTree(function ($obj) {
-	$obj->num = rand(1,100);
-});
-
-var_dump($trunk);
-
-$children = $trunk->getChildren();
-
-foreach ($children as $child) {
-	echo $child->getParent()->num . "\n";
-}
-
-var_dump($child->getAncestors());
-*/
