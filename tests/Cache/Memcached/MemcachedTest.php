728d018b16ec4ba4f693334dc9ec340d74d9550b
David Cloutman <david@liquor.com>
2016-08-24 18:42:01 -0700

    Created unit tests for Memcached singleton wrapper library.

---
+++ tests/Cache/Memcached/MemcachedTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Shoal\Cache\Memcached\MemcachedPool;
+use Shoal\Cache\Memcached\MemcachedSingleton;
+
+class MemcachedTest extends TestCase {
+    /** General test for the MemcachedPool object, which is used to configure the server pool.
+     */
+    public function testMemcachedPool () {
+        $this->assertEquals([], MemcachedPool::getServers());
+
+        MemcachedPool::addServer(); // Sets for likely development values by default.
+        $this->assertEquals([['127.0.0.1', 11211, 100]], MemcachedPool::getServers());
+
+        MemcachedPool::resetServers();
+        $this->assertEquals([], MemcachedPool::getServers());
+
+        return true;
+    }
+
+    /**
+     * @depends testMemcachedPool
+     */
+    public function testMemcachedSingleton ($args) {
+        MemcachedPool::resetServers();
+        MemcachedPool::addServer(); // Setup for a typical local server.
+
+        $mc = MemcachedSingleton::getMemcached();
+
+        $this->assertTrue($mc instanceof \Memcached);
+
+        return $mc;
+    }
+}
