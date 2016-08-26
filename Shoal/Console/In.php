b5b225ec6e9615d5f744b2004d80529d5a8f210c
David Cloutman <david@liquor.com>
2016-08-25 19:52:02 -0700

    Fixed a bug in _toString() where the resource for the STDIN file resource was being returned instead of the value of the user supplied input. Improved comments and made them PHPDoc compatible. Converted variable names to camelCase.

--- Shoal/Console/In.php
+++ Shoal/Console/In.php
@@ -1,31 +1,61 @@
 <?php
+/**
+ * \Shoal\Console\In
+ * @author David Cloutman
+ * @package \Shoal\Console
+ * @license MIT
+ */
+
 namespace Shoal\Console;
 
-/** A simple class to get line input from the console.
+
+/**
+ * A simple class to get line input from the console.
  */
 class In {
+	/**
+	 * @var resource $in A file resource pointed at STDIN.
+	 * @internal
+	 */
 	protected $in;
-	protected $last_input = '';
-	protected $max_len = 8196;
 
+	/**
+	 * @var string $lastInput The last line of input read from STDIN.
+	 * @internal
+	 */
+	protected $lastInput = '';
+
+	/**
+	 * @var integer $maxLen The maximum number of bytes to read from STDIN
+	 * @internal
+	 */
+	protected $maxLen = 8196;
+
+	/**
+	 * Create an new intance.
+	 */
 	public function __construct() {
 		$this->in = fopen('php://stdin', 'r');
 	}
 
-	/** Get a line of input.
-	 *  @param string $prompt An optional line of input.
-	 *  @return string The line of input with leading and trailing whitespace removed.
+	/**
+	 * Get a line of input.
+	 * @param string $prompt An optional line of input.
+	 * @return string The line of input with leading and trailing whitespace removed.
 	 */
 	public function getln ( $prompt = '' ) {
 		echo $prompt;
-		$text = trim(fgets($this->in, $this->max_len));
+		$text = trim(fgets($this->in, $this->maxLen));
 
-		$this->last_input = $text;
-		return $this->last_input;
+		$this->lastInput = $text;
+		return $this->lastInput;
 	}
 
+	/**
+	 * Generates a sanitized string based on the last line of input.
+	 */
 	public function __toString() {
-		return $this->in;
+		return $this->lastInput;
 	}
 }
 
