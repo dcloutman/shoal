8fe187cafafa2c1c868b19eb81c9e1d674ccd111
David Cloutman <david@liquor.com>
2016-08-25 17:57:59 -0700

    Made comments more compatible with PHPDoc. Still throws errors when building documentation, but problem appears to be with PHPDoc.

--- Shoal/F.php
+++ Shoal/F.php
@@ -1,13 +1,22 @@
 <?php
+/**
+ * \Shoal\F
+ * @author David Cloutman
+ * @package \Shoal
+ * @license MIT
+ */
+
 namespace Shoal;
 
-/** A collection of static utility functions.
+/**
+ * A collection of static utility functions.
  */
 class F {
 
-	/** Returns true if a value is an integer or s string representation of an integer.
-	 *  @param mixed $testVar The variable to test.
-	 *  @return boolean true if $testVar is an integer or a string representation of an integer.
+	/**
+	 * Returns true if a value is an integer or s string representation of an integer.
+	 * @param mixed $testVar The variable to test.
+	 * @return boolean true if $testVar is an integer or a string representation of an integer.
 	 */
 	public static function isTruthyInt ( $testVar ) {
 		if (
@@ -22,12 +31,12 @@ class F {
 
 
 	/** Takes an array of arrays that share a common key with a presumed unique value and returns a new array of arrays indexed by that value.
-	 *  @param array $array
+	 *  @param array $arr
 	 *  @param mixed $indexKey
 	 */
-	public static function reindexByMultiValue(array $array, $indexKey) {
+	public static function reindexByMultiValue(array $arr, $indexKey) {
 		$result = [];
-		foreach ($array as $member) {
+		foreach ($arr as $member) {
 			$result[$member[$indexKey]] = $member;
 		}
 
