c7cfa61617dbd928738b1be253fb2d882b066be8
David Cloutman <david@liquor.com>
2016-08-25 17:51:27 -0700

    Improved documentation for the Memchaced library.

--- Shoal/Cache/Memcached/MemcachedPool.php
+++ Shoal/Cache/Memcached/MemcachedPool.php
@@ -1,24 +1,50 @@
 <?php
+/**
+ * \Shoal\Cache\Memcached\MemcachedPool
+ * @author David Cloutman
+ * @package \Shoal\Cache\Memcached
+ * @license MIT
+ */
+
 namespace Shoal\Cache\Memcached;
 
-/** Configuration of MemcachedFactory is done through the MemcachedPool class. For development purposes, a single call to
- *  MemcachedPool::addServer() with no parameters is sufficient. Multiple calls will build out the pool. These calls should 
- *  generally be made in the configuration or bootstrapping section of the application prior to the first call of
- *  MemcachedFactory::getMemcached().
+/** 
+ * Configuration of MemcachedFactory is done through the MemcachedPool class. For development purposes, a single call to
+ * MemcachedPool::addServer() with no parameters is sufficient. Multiple calls will build out the pool. These calls should 
+ * generally be made in the configuration or bootstrapping section of the application prior to the first call of
+ * MemcachedFactory::getMemcached().
  */
 class MemcachedPool {
+	/**
+	 * @var array $servers
+	 * @internal
+	 */
 	protected static $servers = [];
 
+	/**
+	 * Add a server to the pool.
+	 * @param string $host The domain or IP address of the server.
+	 * @param integer $port The port of the server.
+	 * @param integer $weight
+	 */
 	public static function addServer($host = '127.0.0.1', $port = 11211, $weight = 100) {
 		// 'I.P. Address', Port, Weight
 		self::$servers[] = [$host, $port, $weight];
 	}
 
 
+	/**
+	 * Returns the entire server pool configuration.
+	 * @param array
+	 */
 	public static function getServers () {
 		return self::$servers;
 	}
 
+	/**
+	 * Remove all servers from the server pool configuration.
+	 * @return array
+	 */
 	public static function resetServers () {
 		self::$servers = [];
 	}
--- Shoal/Cache/Memcached/MemcachedSingleton.php
+++ Shoal/Cache/Memcached/MemcachedSingleton.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * \Shoal\Cache\Memcached\MemcachedSingleton
+ * @author David Cloutman
+ * @package \Shoal\Cache\Memcached
+ * @license MIT
+ */
+
 namespace Shoal\Cache\Memcached;
 
 
@@ -9,31 +16,93 @@ namespace Shoal\Cache\Memcached;
  */
 class MemcachedSingleton {
 	// These timeframes increment by factors of 4. In practice, these tend to be useful in terms of fine tuning cache times.
+
+	/**
+	 * @var integer CACHE_ULTRASHORT 75 seconds
+	 * @api
+	 */
 	const CACHE_ULTRASHORT = 75;
-	const CACHE_SHORT = 300; // 5 minutes
-	const CACHE_MEDIUM = 1200; // 20 minutes
-	const CACHE_LONG = 4800;	// 1 hour, 20 minutes
-	const CACHE_ULTRALONG =  19200; // 5 hours, 20 minutes
+
+	/**
+	 * @var integer CACHE_SHORT 5 minutes
+	 * @api
+	 */
+	const CACHE_SHORT = 300;
+
+	/**
+	 * @var integer CACHE_MEDIUM 20 minutes
+	 * @api
+	 */
+	const CACHE_MEDIUM = 1200;
+
+	/**
+	 * @var integer CACHE_LONG 1 hour, 20 minutes
+	 * @api
+	 */
+	const CACHE_LONG = 4800;
+
+	/**
+	 * @var integer CACHE_ULTRALONG 5 hours, 20 minutes
+	 * @api
+	 */
+	const CACHE_ULTRALONG =  19200;
+
 
 	// These are based on time frames defined by Memcached itself.
-	const CACHE_MAX = 2592000; // 30 days, the Memcached maximum.
-	const CACHE_INFINITY = 0; // Memcached interprets 0 as forever.
+	/**
+	 * @var CACHE_MAX 30 days, the Memcached maximum.
+	 * @api
+	 */
+	const CACHE_MAX = 2592000;
+	
+	/**
+	 * @var CACHE_INFINITY Memcached interprets 0 as forever.
+	 * @api
+	 */
+	const CACHE_INFINITY = 0;
 
+	
 	// These standard time periods are useful when communicating with your boss.
+	
+	/**
+	 * @var integer CACHE_MINUTE
+	 * @api
+	 */
 	const CACHE_MINUTE = 60;
+	
+	/**
+	 * @var integer CACHE_HOUR
+	 * @api
+	 */
 	const CACHE_HOUR = 3600;
+	
+	/**
+	 * @var integer CACHE_DAY
+	 * @api
+	 */
 	const CACHE_DAY = 14400;
+	
+	/**
+	 * @var integer CACHE_WEEK
+	 * @api
+	 */
 	const CACHE_WEEK = 604800;
 
 
-	// The singleton instance of the Memcached class.
+	/**
+	 * @var \Memcached The singleton instance of the Memcached class.
+	 * @internal
+	 */
 	private static $memcachedInstance = null;
 
-	// No, you may not instantiate this class.
+	/**
+	 * No, you may not instantiate this class.
+	 */
 	private function __construct () {}
 
-	/** Returns a single instance of Memcached using the servers configured in MemcachedPool.
-	 *  @return Memcached
+	/**
+	 * Returns a single instance of Memcached using the servers configured in MemcachedPool.
+	 * @return Memcached
 	 */
 	public static function getMemcached () {
 		if ( null === self::$memcachedInstance ) {
