0a648f97e32e97148f1b79c7593173471bc9cfe8
David Cloutman <david.cloutman@lampfire.com>
2016-08-29 10:43:39 -0700

    Implemented getAttributeString() method on Br and Text are classes in the Ui library.

--- Shoal/Ui/Br.php
+++ Shoal/Ui/Br.php
@@ -20,33 +20,23 @@ class Br extends Element {
 		$this->element_name = 'br';
 	}
 
+	/**
+	 * Returns a string of attibutes. Can be called by inheriting classes for consistent behavior.
+	 * @return string A string of attributes for use in an opening or self closing HTML tag.
+	 */
+	public function getAttributeString () {
+		$attributeString = '';
+		$attributeString .= parent::getAttributeString();
+		return $attributeString;
+	}
+
 	/** 
 	 * Get HTML fragment.
 	 * @return string
 	 */
 	public function __toString () {
 		$string_value = "<{$this->element_name} "; 
-
-		if ( !empty( $this->name ) ) {
-			$string_value .= "name=\"{$this->name}\" ";
-		}
-
-		if ( !empty( $this->id ) ) {
-			$string_value .= "id=\"{$this->id}\" ";
-		}
-
-		if ( !empty( $this->type ) ) {
-			$string_value .= "type=\"{$this->type}\" ";
-		}
-
-		if ( !empty( $this->class ) ) {
-			$string_value .= "class=\"{$this->class}\" ";
-		}
-
-		if ( !empty( $this->style ) ) {
-			$string_value .= "style=\"{$this->style}\" ";
-		}
-
+		$string_value .= $this->getAttributeString();
 		$string_value .= '/>';
 
 		return $string_value;
