df7970038c5e4a1848cc8d810ca2ca050004e5bb
David Cloutman <david@liquor.com>
2016-09-21 22:19:02 -0700

    Added a submit type for the <input /> element in the Ui library.

---
+++ Shoal/Ui/SubmitInput.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * \Shoal\Ui\SubmitInput
+ * @author David Cloutman
+ * @package \Shoal\Ui
+ * @license MIT
+ */
+
+namespace Shoal\Ui;
+
+/** Creates a submit button for a form.
+ */
+class SubmitInput extends TextInput {
+
+	/**
+	 * Create an SubmitInput object.
+	 */
+	function __construct() {
+		parent::__construct();
+
+		$this->type = 'submit';
+	}
+
+}
