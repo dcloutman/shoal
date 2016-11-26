f8870fcb18084c995f437f43531cef614094b52c
David Cloutman <david@liquor.com>
2016-11-26 15:52:54 -0800

    Improved comments for that PHPDocs can be generated.

--- Shoal/Slim/Net/Json/Utilities.php
+++ Shoal/Slim/Net/Json/Utilities.php
@@ -1,15 +1,23 @@
 <?php
+/**
+ * \Shoal\Slim\Net\Json
+ * @author David Cloutman
+ * @package \Shoal\Slim\Net
+ * @license MIT
+ */
+
 namespace Shoal\Slim\Net\Json;
 
 /**
- * Static method userful for using JSON in a Slim application.
+ * A static class with methods userful for using JSON in a Slim application.
  */
 class Utilities {
 
-	/** Configure the Slim->response object for JSON content.
-	 *  @param \Slim\Slim $app An instance of the Slim application.
-	 *  @param integer $statusCode A valid HTTP status code.
-	 *  @
+	/**
+	 * Configure the Slim->response object for JSON content.
+	 * @param \Slim\Slim $app An instance of the Slim application.
+	 * @param integer $statusCode A valid HTTP status code.
+	 * @param \Shoal\Net\Json\AjaxRespon $json A JSON response object.
 	 */
 	public static function configureJsonResponse ( \Slim\Slim $app, $statusCode, \Shoal\Net\Json\AjaxResponse $json ) {
 		$app->response->setStatus( $statusCode );
