d67b3f09c1ce00d1e005ef1c56183f1e3fcbe25f
David Cloutman <david.cloutman@lampfire.com>
2017-09-27 12:43:31 -0700

    Altered comments in the Users.sql header to eliminate mention encouraging use of the sha1 hashing algorithm. Attempted to better explain the table's design rationale and intended integration with Shoal's password hashing algorithm.

--- sql-lib/tables/Users.sql
+++ sql-lib/tables/Users.sql
@@ -1,11 +1,26 @@
--- This table is designed to store users of a typical Web application. Passwords
--- are intended to by sha1 hashed using a concatination of an application salt, the value in the user_salt field (see the urandom library to create a value)
--- and the plaintext password. DO NOT STORE THE PLAIN TEXT PASSWORD IN THE `password_hash`
--- FIELD! 
+-- This table is designed to store users of a typical Web application. It is meant to be a
+-- starting point for writing a secure application, not a complete solution.
 --
--- In this table, User types are stored are specified as an enum. This table can
--- be modified to specify different types of users, or a foreign key can be added
--- so that user types can be specified in another table. User types may also be ommited altogether
+-- PASSWORDS
+--
+-- Passwords are intended to be hashed using an one way algorithm such as 
+-- Shoal\Auth\PasswordUtils::generatePasswordHash(). The strategy anticipated by this table would
+-- use a concatination of an application salt, the value in the user_salt field, which can be
+-- generated with Shoal\Auth\PasswordUtils::generateSalt() and the plaintext password.
+--
+-- Passwords stored this way are, by design, irretrievable. Applications that require users to
+-- create a new password when the old one is forgotten are typically more secure than those
+-- that provide a forgotten password to the user. This table reflects that architecture bias.
+--
+-- *** DO NOT STORE THE PLAIN TEXT PASSWORD IN THE `password_hash` FIELD! ***
+--
+-- By default Shoal\Auth\PasswordUtils::generatePasswordHash() provides multiple layers of hashing
+-- and obfuscation through the use of a native salting algorithm, following the above strategy,
+-- which is then hashed again using the bcrypt algorithm, which provides its own additional salt.
+--
+-- In this table, User types are stored are specified as an enum. This table can be modified to 
+-- specify different types of users, or a foreign key can be added so that user types can be 
+-- specified in another table. User types may also be ommited altogether
 -- and if multiple roles are to be assigned to a user, a soft entity can be created
 -- with Users.user_id being used as the foreign key referencing the Users table.
 
